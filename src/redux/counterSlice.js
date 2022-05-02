395a8b5a5ff44df4589698b54b571ec2b7187792
yuhsin828 <71396690+yuhsin828@users.noreply.github.com>
2022-05-02 13:36:07 +0800

    done

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+// Part1: Define Slice (including reducers and actions)
+const initialState = { counterValue: 0 };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    // setCounter: (state, action) => {
+    //   state.counterValue = action.payload;
+    // },
+    increaseOne: (state, action) => {
+      state.counterValue = action.payload;
+    },
+    decreaseOne: (state, action) => {
+      state.counterValue = action.payload;
+    },
+  },
+});
+
+// export state to global
+export const selectCounter = (state) => state.counter.counterValue;
+
+// export actions to global
+// export const { setCounter } = counterSlice.actions;
+export const { increaseOne } = counterSlice.actions;
+export const { decreaseOne } = counterSlice.actions;
+
+// export reducer to global
+export default counterSlice.reducer;
